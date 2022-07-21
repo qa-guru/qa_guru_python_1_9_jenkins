73ed72a80e891c0c107f7e4955c7653997695b3b
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2022-07-21 20:36:19 +0300

    add add_video function with selenoid records

--- tests/demoqa/test_registration_form.py
+++ tests/demoqa/test_registration_form.py
@@ -15,14 +15,15 @@ def test_successful():
         "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
-            "enableVideo": False
+            "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
 
     driver = webdriver.Remote(
         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options)
+        options=options
+    )
     browser.config.driver = driver
 
     first_name = "Alex"
--- utils/attach.py
+++ utils/attach.py
@@ -15,3 +15,11 @@ def add_logs(browser):
 def add_html(browser):
     html = browser.driver.page_source
     allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
+
+
+def add_video(browser):
+    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
+    html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
+           + video_url \
+           + "' type='video/mp4'></video></body></html>"
+    allure.attach(html, 'video' + browser.driver.session_id, AttachmentType.HTML, '.html')
