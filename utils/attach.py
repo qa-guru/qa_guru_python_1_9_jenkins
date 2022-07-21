5779d679fff106b4f261b0eae43aff167fad2e99
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2022-07-21 20:08:06 +0300

    add allure attachment util

---
+++ utils/attach.py
@@ -0,0 +1,17 @@
+import allure
+from allure_commons.types import AttachmentType
+
+
+def add_screenshot(browser):
+    png = browser.driver.get_screenshot_as_png()
+    allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
+
+
+def add_logs(browser):
+    log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
+    allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
+
+
+def add_html(browser):
+    html = browser.driver.page_source
+    allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
